487001aa634e9be29edc3a00f1a2b04f505ba9aa
EMCgaming <63981925+EMCgaming@users.noreply.github.com>
2021-06-12 14:37:04 +0300

    upgraded Parts.User_Input.User_Input class

--- Classes/User_Input.py
+++ Classes/User_Input.py
@@ -2,18 +2,36 @@ import pygame
 
 
 class User_Input:
-    def __init__(self, WIDTH, HEIGHT, x, y, width, height, color, WIN, font_size):
+    def __init__(self, WIDTH: int, HEIGHT: int, x: int, y: int, width: int, height: int,
+                 base_color: tuple, WIN: pygame.Surface, text_color: tuple, font_size: int):
+        """
+        Parameters
+        ----------
+
+         WIDTH: int
+         HEIGHT: int
+         x: int
+         y: int
+         width: int
+         height: int
+         base_color: tuple
+         WIN: pygame.Surface
+         text_color: tuple
+         font_size: int
+        """
         self.WIDTH = WIDTH
         self.HEIGHT = HEIGHT
         self.x = x
         self.y = y
         self.height = height
         self.width = width
-        self.color = color
+        self.base_clr = base_color
         self.WIN = WIN
+        self.text_clr = text_color
         self.font_size = font_size
         self.font = pygame.font.SysFont("comicsans", self.font_size)
         self.text = ""
+        self.antialias = True
 
     def draw(self):
         """
@@ -21,8 +39,8 @@ class User_Input:
         :return: None
         """
         # pygame.draw.rect(pygame.Surface, color: tuple, (x, y, width, height))
-        pygame.draw.rect(self.WIN, self.color, (self.x, self.y, self.width, self.height))
-        text = self.font.render(str(self.text), 1, (255, 255, 255))
+        pygame.draw.rect(self.WIN, self.base_clr, (self.x, self.y, self.width, self.height))
+        text = self.font.render(str(self.text), 1, self.text_clr)
         self.WIN.blit(text, (
                             self.WIDTH/2 - text.get_width()/2,
                             self.y/2 + text.get_height()/2
@@ -91,3 +109,67 @@ class User_Input:
             return 9
         else:
             return False
+
+    def insert_letter(self, ltr):
+        """
+        inserts the letter at the end of the text
+        :param ltr: str
+        :return: None
+        """
+        self.text += ltr
+
+    def write(self, character):
+        """
+        writes a character to the field
+        :param character: any
+        :return: None
+        """
+        self.text += character
+
+    def clear(self):
+        """
+        clears the tect that is writen
+        :return: None
+        """
+        self.text = ""
+
+    def delete_last(self):
+        """
+        deletes the last character of the text
+        :return: None
+        """
+        self.text = self.text[:-1]
+
+    def get_text(self):
+        """
+        it gives the current text of the field
+        :return: self.text: str
+        """
+        return self.text
+
+    def is_empty(self):
+        """
+        tells if the field is empty
+        :return: bool
+        """
+        return self.text == ""
+
+    def text_color(self, color: tuple):
+        """
+        sets the color for the text
+        :param color: tuple
+        :return: None
+        """
+        self.text_clr = color
+
+    def base_color(self, color: tuple):
+        """
+        sets the color of the base field color
+        :param color:
+        :return: None
+        """
+        self.base_clr = color
+
+    def events(self, event):
+        if event.type == pygame.K_BACKSPACE:
+            self.delete_last()

af7a5b4daf0bb13528f8f90e550ffd1e6ecdb3a3
EMCgaming <63981925+EMCgaming@users.noreply.github.com>
2021-06-13 00:41:06 +0300

    removed escape method from GUI class

--- Classes/GUI.py
+++ Classes/GUI.py
@@ -18,17 +18,6 @@ class GUI:
     def draw(self):
         self.WIN.fill(self.BASE_COLOR)
 
-    def escape(self):
-        """
-        check if the player has quited the window
-        :return: Bool
-        """
-        for event in pygame.event.get():
-            keys = pygame.key.get_pressed()
-            if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
-                return True
-        return False
-
     def set_window(self, TITLE: str):
         pygame.display.set_caption(TITLE)
         # pygame.display.set_icon("icon")

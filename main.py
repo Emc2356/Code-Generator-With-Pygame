6a19cd5d6197b8333fc5075d97bde9c8cb4226ee
EMCgaming <63981925+EMCgaming@users.noreply.github.com>
2021-06-16 13:10:20 +0300

    moved the save button for extra space for the view button

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ COPY_BUTTON = Button(x=round(WIDTH/2 - 250/2), y=HEIGHT - 70, width=250, height=
                      text="click to copy", text_color=COLORS["BLACK"], color=COLORS["BLUE"])
 REG_BUTTON = Button(x=round(WIDTH/2 - 340/2), y=HEIGHT - 115, width=340, height=40, WIN=WIN,
                     text="click to regenerate the password", text_color=COLORS["BLACK"], color=COLORS["RED"], font_size=30)
-save_button = Button(x=12, y=HEIGHT - 70, width=100, height=50,
+save_button = Button(x=WIDTH - COPY_BUTTON.get_width()/2 + 10, y=HEIGHT - 70, width=100, height=50,
                      WIN=WIN, text="save", text_color=COLORS["BLACK"], color=COLORS["GREEN"], font_size=60)
 save_save_button = Button(x=10, y=385, width=100, height=50, WIN=WIN,
                           text_color=COLORS["BLACK"], color=COLORS["GREEN"], text="save", font_size=60)
@@ -39,8 +39,7 @@ WD = Window_Buttons(WIDTH, HEIGHT, WIN)
 bar = User_Input(WIDTH, HEIGHT, round(WIDTH/2 - 150/2), 20, 150, 50, COLORS["BLACK"], WIN, COLORS["WHITE"], 50)
 save_input_field = User_Input(WIDTH, HEIGHT, 10, 50, WIDTH - 20, 50, (0, 0, 0), WIN, (255, 255, 255), 60)
 save_back_button = Button(COLORS["RED"], 10, 440, 100, 50, WIN, COLORS["BLACK"], "Go back", font_size=35)
-dark_mode = Dark_Mode(WIN, [pygame.image.load("assets/mode-assets/dark-32.png"), pygame.image.load(
-    "assets/mode-assets/bright-32.png")],
+dark_mode = Dark_Mode(WIN, [pygame.image.load("assets/mode-assets/dark-32.png"), pygame.image.load("assets/mode-assets/bright-32.png")],
                       [(255, 255, 255), (30, 30, 30)], WIDTH - (32 + 5), 5)
 clock = pygame.time.Clock()
 FPS = 60
